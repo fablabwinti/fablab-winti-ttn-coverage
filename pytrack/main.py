8af58b3feca619bab1bdb75c6d39578d8174b20c
Christoph Heer <christoph.heer@hotmail.ch>
2017-08-05 10:13:52 +0200

    ...

--- pytrack/main.py
+++ pytrack/main.py
@@ -25,18 +25,15 @@ def send(socket, data, color = 0x007f00):
     time.sleep(0.5)
     pycom.rgbled(0x000000)
 
-
 pycom.heartbeat(False)
 
 # Initialize LoRa in LORAWAN mode.
 lora = LoRa(mode=LoRa.LORAWAN)
 
-
-
 # create an OTA authentication params
-dev_eui = binascii.unhexlify('....'.replace(' ','')) # these settings can be found from TTN
-app_eui = binascii.unhexlify('....'.replace(' ','')) # these settings can be found from TTN
-app_key = binascii.unhexlify('....'.replace(' ','')) # these settings can be found from TTN
+dev_eui = binascii.unhexlify('00C435FB8460DBE0') # these settings can be found from TTN
+app_eui = binascii.unhexlify('70B3D57EF000621E') # these settings can be found from TTN
+app_key = binascii.unhexlify('C520CE7D05798E4F19A0BA121E1402CF') # these settings can be found from TTN
 
 # set the 3 default channels to the same frequency (must be before sending the OTAA join request)
 lora.add_channel(0, frequency=868100000, dr_min=0, dr_max=5)
@@ -66,68 +63,59 @@ s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 s.setblocking(False)
 
 py = Pytrack()
-l76 = L76GNSS(py, timeout=40) # GSP timeout set to 120 seconds
+l76 = L76GNSS(py, timeout=30) # GSP timeout set to 120 seconds
 
 last_lat = 0.0
 last_lon = 0.0
+data_log_size = 4
 
-msg_log = None
-msg_count = 0
-msg_log_length = 5
-
-nomsg_count = 0
+data_log = []
 
 while True:
 
     print('read gps data')
     lat, lon = l76.coordinates();
+    #lat = 48.12345678
+    #lon = 8.12345678
 
     if lat != None and  lon != None:
 
-        if lat != last_lat and lon != last_lon:
+        if True or lat != last_lat and lon != last_lon:
 
-            msgType = [1]
+            msgType = [2]
+
             lat_b = list(struct.pack("!f", lat))
             lon_b = list(struct.pack("!f", lon))
             alt_b = list(struct.pack('<H', 444))
-            data = msgType + lat_b + lon_b + alt_b
 
+            data_log_lenth = [len(data_log)]
+            data = msgType + lat_b + lon_b + alt_b + data_log_lenth
 
-            print('send msg')
-            print(data)
+            for li in data_log:
+                data = data + list(struct.pack("!f", li[0])) + list(struct.pack("!f", li[1]))
+
+            #print('send msg')
+            #print(data)
 
             send(s, bytes(data))
 
-            if msg_count == 0:
-                msg_log = [10, msg_log_length]
+            #print('sent bytes:' + str(len(data)) )
 
-            msg_log = msg_log + lat_b + lon_b
-            msg_count = msg_count + 1
-            if msg_count == msg_log_length:
-                time.sleep(30)
-                send(s, bytes(msg_log), color=0x00007f)
-                msg_count = 0
-                msg_log = None
-                time.sleep(15)
-            else:
-                time.sleep(45)
+            data_log.insert(0, [lat, lon])
+            if len(data_log) > data_log_size:
+                data_log.pop()
+
 
             last_lat = lat
             last_lon = lon
 
-            nomsg_count = 0
+            time.sleep(40)
+
         else:
-            print('same coordinates')
-            nomsg_count = nomsg_count + 1
-            time.sleep(30)
+            #print('same coordinates')
+            time.sleep(5)
 
     else:
-        print('invalid gps data')
+        #print('invalid gps data')
         blink(0x7f0000, 1, 3)
-        nomsg_count = nomsg_count + 1
         time.sleep(5)
-
-    if nomsg_count >= 20:
-        print('send ping')
-        send(s, bytes([128]))
-        nomsg_count = 0
