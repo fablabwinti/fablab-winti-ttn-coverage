92d0e0b5e3f518c546a49d3e6f75fe9b243ee761
Christoph Heer <christoph.heer@hotmail.ch>
2017-08-05 10:14:12 +0200

    ..

--- pytrack/main.py
+++ pytrack/main.py
@@ -31,9 +31,9 @@ pycom.heartbeat(False)
 lora = LoRa(mode=LoRa.LORAWAN)
 
 # create an OTA authentication params
-dev_eui = binascii.unhexlify('00C435FB8460DBE0') # these settings can be found from TTN
-app_eui = binascii.unhexlify('70B3D57EF000621E') # these settings can be found from TTN
-app_key = binascii.unhexlify('C520CE7D05798E4F19A0BA121E1402CF') # these settings can be found from TTN
+dev_eui = binascii.unhexlify('..') # these settings can be found from TTN
+app_eui = binascii.unhexlify('..') # these settings can be found from TTN
+app_key = binascii.unhexlify('..') # these settings can be found from TTN
 
 # set the 3 default channels to the same frequency (must be before sending the OTAA join request)
 lora.add_channel(0, frequency=868100000, dr_min=0, dr_max=5)
